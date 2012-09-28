b7ae11b71c3c5106c491291e2164da7c103a58c3
Subho Banerjee <subs.zero@gmail.com>
2012-09-28 19:38:32 +0530

    handling 404

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express'),
     routes = require('./routes');
 
 var app = module.exports = express.createServer();
 
 // Configuration
-
 app.configure(function(){
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
+    app.set("view options", { layout: false })
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(express.cookieParser());
     app.use(express.session({ secret: 'visualWorkflow@IIITH' }));
     app.use(app.router);
     app.use(express.static(__dirname + '/public'));
+    app.use(function(req, res, next){
+        if (req.accepts('html')) {
+            res.status(404);
+            res.render('404', { url: req.url });
+            return;
+        }
+        if (req.accepts('json')) {
+            res.send({ error: 'Not found' });
+            return;
+        }
+        res.type('txt').send('Not found');
+    });
 });
 
 app.configure('development', function(){
@@ -30,9 +37,14 @@ app.configure('production', function(){
 });
 
 // Routes
-
 app.get('/proxy', routes.proxy);
 
+
+// Bind
 app.listen(3000, function(){
-    console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+    console.log("Express server listening on port %d in %s mode\n", app.address().port, app.settings.env);
+    if(app.settings.env === 'development') {
+        console.log('Starting browser');
+        require('./browser').open('http://localhost:'+app.address().port);
+    }
 });
---
+++ browser.js
@@ -0,0 +1,13 @@
+
+var child_process = require('child_process');
+
+// Open file in default browser
+exports.open = function(file) {
+	var browser;
+	switch(process.platform) {
+		case "win32": browser = "start"; break;
+		case "darwin": browser = "open"; break;
+		default: browser = "xdg-open"; break;
+	}
+	child_process.spawn(browser, [file]);
+}
