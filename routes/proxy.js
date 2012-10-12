f2ffb235801b084d6f0019ad7f332c99066694f7
Subho Banerjee <subs.zero@gmail.com>
2012-10-12 14:28:17 +0530

    adding error handling

--- routes/proxy.js
+++ routes/proxy.js
@@ -3,10 +3,15 @@
 var request = require('request');
 
 exports.proxify = function (req, res) {
-    var options = JSON.parse(req.body.options);
-    request(options, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            res.send(body);
-        }
-    });
+    var options = null;
+    if (req.body.options) {
+        var options = JSON.parse(req.body.options);     
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                res.send(body);
+            }
+        });
+    } else {
+        res.status(500).send({error: 'No options specified in HTTP request'});
+    }
 };
