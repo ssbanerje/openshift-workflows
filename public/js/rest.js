93a9c89ef45ba43e8f05f566c6ecb0a84d963caf
Subho Banerjee <subs.zero@gmail.com>
2012-10-13 12:05:14 +0530

    better class structure for the rest object

--- public/js/rest.js
+++ public/js/rest.js
@@ -1,47 +1,19 @@
-// The Rest interface
-var Rest = {
-    // Authenticationd details
-    username: '',
-    password: '',
-    domain: '',
-    api: {},
+/**
+*
+* The REST interface to OpenShift
+*
+*/
+var Rest = function(dom) {
+    var username = '', // Openshift username
+        password = '', // Openshift password
+        domain = dom; // Domain of openshift broker
+    this.api = {}; // API of the Broker 
     
-    api: function (domain) { // Get the API reference
-        var data = {
-            uri: domain+'/broker/rest/api',
-            headers: {
-                accept: 'application/json'
-            },
-            method: 'GET'
-        };
-        var callback = function (d) {
-            Rest.api = d;
-            Rest.domain = domain;
-        };
-        Rest.proxify(data, callback);
-    },
+    var authString = function (username, password) { // Get the user authentcation/authorzation string
+        return 'Basic ' + window.btoa(username + ':' + password);
+    };
     
-    authenticate: function (username, password) { // Authenticate user on domain
-        var data = {
-            uri: Rest.domain+'/broker/rest/domains',
-            headers: {
-                accept: 'application/json',
-                Authorization: Rest.authString(username, password)
-            },
-            method: 'GET'
-        };
-        var callback = function (d) {
-            Rest.username = username;
-            Rest.password = password;
-        };
-        Rest.proxify(data, callback);
-    },
-    
-    authString: function (username, password) { // Get the user authentcation/authorzation string
-        return 'Basic '+window.btoa(username+':'+password);
-    },
-    
-    proxify: function (data, callback) { // Send data to host through the proxy
+    var proxify = function (data, callback) { // Send data to host through the proxy
         $.ajax({
             'url': '/proxy',
             'dataType': 'json',
@@ -53,5 +25,35 @@ var Rest = {
                 callback(data);
             }
         });
-    }
+    };
+    
+    this.getApi = function () { // Get the API reference
+        var data = {
+            uri: domain+'/broker/rest/api',
+            headers: {
+                accept: 'application/json'
+            },
+            method: 'GET'
+        };
+        var callback = function (d) {
+            this.api = d;
+        };
+        proxify(data, callback);
+    };
+    
+    this.authenticate = function (u, p) { // Authenticate user on domain
+        var data = {
+            uri: domain+'/broker/rest/domains',
+            headers: {
+                accept: 'application/json',
+                Authorization: authString(u, p)
+            },
+            method: 'GET'
+        };
+        var callback = function (d) {
+            username = u;
+            password = p;
+        };
+        proxify(data, callback);
+    };
 };
