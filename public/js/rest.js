6bc8064a3c68afb3eef058fb513585a4c48d5642
Subho Banerjee <subs.zero@gmail.com>
2012-10-13 11:38:19 +0530

    basic authentication

--- public/js/rest.js
+++ public/js/rest.js
@@ -1,31 +1,57 @@
-// Send data to host through the proxy
-function proxify(data, callback) {
-    $.ajax({
-        'url': '/proxy',
-        'dataType': 'json',
-        'type': 'POST',
-        'data': {
-            options: JSON.stringify(data)
-        },
-        'success': function (data, textStatus, jqXHR) {
-            callback(data);
-        }
-    });
-}
-
-// The required rest interface
+// The Rest interface
 var Rest = {
-    api : function () {
+    // Authenticationd details
+    username: '',
+    password: '',
+    domain: '',
+    api: {},
+    
+    api: function (domain) { // Get the API reference
         var data = {
-            uri: 'https://openshift.redhat.com/broker/rest/api',
+            uri: domain+'/broker/rest/api',
             headers: {
                 accept: 'application/json'
             },
             method: 'GET'
         };
         var callback = function (d) {
-            console.log(d);
+            Rest.api = d;
+            Rest.domain = domain;
         };
-        proxify(data, callback);
+        Rest.proxify(data, callback);
+    },
+    
+    authenticate: function (username, password) { // Authenticate user on domain
+        var data = {
+            uri: Rest.domain+'/broker/rest/domains',
+            headers: {
+                accept: 'application/json',
+                Authorization: Rest.authString(username, password)
+            },
+            method: 'GET'
+        };
+        var callback = function (d) {
+            Rest.username = username;
+            Rest.password = password;
+        };
+        Rest.proxify(data, callback);
+    },
+    
+    authString: function (username, password) { // Get the user authentcation/authorzation string
+        return 'Basic '+window.btoa(username+':'+password);
+    },
+    
+    proxify: function (data, callback) { // Send data to host through the proxy
+        $.ajax({
+            'url': '/proxy',
+            'dataType': 'json',
+            'type': 'POST',
+            'data': {
+                options: JSON.stringify(data)
+            },
+            'success': function (data, textStatus, jqXHR) {
+                callback(data);
+            }
+        });
     }
 };
