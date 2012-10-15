b52d3b7d1fc5f80f9da1ba70deab55551b503819
Subho Banerjee <subs.zero@gmail.com>
2012-10-16 00:56:35 +0530

    better angularjs support

--- public/js/page.js
+++ public/js/page.js
@@ -5,114 +5,111 @@
 *
 */
 
-var Page = {
-    State : { // The page state
-        rapi: undefined,
-        connected: false
-    },
+function setError(text) { // Set the global error in the application
+    $('#errorPlaceHolder').html('<div class="alert alert-error fade in">' + text
+                                + '<a class="close" data-dismiss="alert" href="#">&times;</a></div>');
+}
 
-    setError : function SetError(text) { // Set the global error in the application
-        $('#errorPlaceHolder').html('<div class="alert alert-error fade in">' + text
-                                    + '<a class="close" data-dismiss="alert" href="#">&times;</a></div>');
-    },
+var workflows = angular.module('workflows', []);
 
-    spinner: undefined, // Show a spinner to indicate busy status
-    startSpinner : function () {
-        $('#spinner').html('&nbsp;&nbsp;');
-        if(!this.spinner) {
-            this.spinner = new Spinner({
-                lines: 9, length: 3, width: 3, radius: 4, corners: 1, rotate: 0,
-                color: '#fff', speed: 1.3, trail: 100, shadow: false, className: 'spinner',
-                zIndex: 2e9, top: '5px', left: 'auto'
-            }).spin(document.getElementById('spinner'));
-        } else {
-            this.spinner.spin(document.getElementById('spinner'));
+var App = function ($scope, $http) {
+    // Page related objects 
+    var Busy = {  // Show a spinner to indicate busy status
+        spinner: undefined,
+        
+        start : function () {
+            $('#spinner').html('&nbsp;&nbsp;');
+            if(!this.spinner) {
+                this.spinner = new Spinner({
+                    lines: 9, length: 3, width: 3, radius: 4, corners: 1, rotate: 0,
+                    color: '#fff', speed: 1.3, trail: 100, shadow: false, className: 'spinner',
+                    zIndex: 2e9, top: '5px', left: 'auto'
+                }).spin(document.getElementById('spinner'));
+            } else {
+                this.spinner.spin(document.getElementById('spinner'));
+            }
+        },
+        
+        stop : function () {
+            this.spinner.stop();
+            $('#spinner').html('<i class="icon-cloud"></i>');
         }
-    },
-    stopSpinner : function () {
-        this.spinner.stop();
-        $('#spinner').html('<i class="icon-cloud"></i>');
-    }
-};
-
-// Communicate between controllers
-var angApp = angular.module('workflows', []);
-angApp.factory('messageBoard', function ($rootScope) {
-    var board = {};
-    board.cartridges = [];
-    board.broadcastCartridges = function (cartridges) {
-        this.cartridges = cartridges;
-        this.pushCartridges();
     };
-    board.pushCartridges = function () {
-        $rootScope.$broadcast('newCartridgesListed');
-    };
-    return board;
-});
-
-// The AngularJS Controller for the connection parameters
-var ConnectionParams = function ($scope, messageBoard) {
+    
+    // Variables related to the connection parameters
     $scope.host = 'https://openshift.redhat.com';
     $scope.username = '';
     $scope.password = '';
+    $scope.authString = '';
     $scope.appName = '';
     $scope.namespace = '';
-
-    $scope.submit = function () { // Check the connection details and get the cartridge list
-        Page.State.connected = false;
+    $scope.connected = false;
+    
+    // Variables related to the cartridges
+    $scope.cartridges = [];
+    
+    // Functions dealing with the connection parameters
+    $scope.submit = function () { // Authenticate user and get the list of cartridges
+        Busy.start();
+        $('#connectionModal').modal('hide');
         $('#cartridges').hide();
         $(".alert").alert('close');
         $('#connection').css('color', '#d00');
-        messageBoard.broadcastCartridges([]);
-        Page.startSpinner();
-        var restObj = new Rest($scope.host);
-        restObj.getApi();
-        restObj.authenticate($scope.username, $scope.password);
-        restObj.getCartridges();
-        var interval = setInterval(function () {
-            if (restObj.connected && !Page.State.connected) {
-                Page.State.connected = true;
-                Page.State.rapi = restObj;
-                messageBoard.broadcastCartridges(Page.State.rapi.cartridges);
+        $scope.cartridges = [];
+        var errorCallback = function (data, status, headers, config) {
+            Busy.stop();
+            switch(status) {
+                    case 401:
+                        setError('Incorrect <strong>username</strong> or <strong>password</strong> entered');
+                        break;
+                    default:
+                        setError('Error in contacting server!');
+                        break;
+            }
+        };
+        $http({ // Authenticate user
+            url: '/proxy',
+            method: 'POST',
+            dataType: 'json',
+            data: {
+                options: JSON.stringify({
+                    uri: $scope.host + '/broker/rest/user',
+                    headers: {
+                        accept: 'application/json',
+                        Authorization: 'Basic ' + window.btoa($scope.username + ':' + $scope.password)
+                    },
+                    method: 'GET'
+                })
+            }
+        }).success(function (data, status, headers, config) {
+            $scope.authString = 'Basic ' + window.btoa($scope.username + ':' + $scope.password);
+            $scope.connected = true;
+            $http({ // Get the list of cartridges
+                url: '/proxy',
+                method: 'POST',
+                dataType: 'json',
+                data: {
+                    options: JSON.stringify({
+                        uri: $scope.host + '/broker/rest/cartridges',
+                        headers: {
+                            accept: 'application/json'
+                        },
+                        method: 'GET'
+                    })
+                }
+            }).success(function (data, status, headers, config) {
+                var i;
+                for (i = 0; i < data.data.length; i = i + 1) {
+                    data.data[i].img = 'http://placehold.it/120x80'
+                }
+                $scope.cartridges = data.data;
                 $('#connection').css('color', '#0d0');
                 $('#cartridges').show();
-                Page.stopSpinner();
-            }
-        }, 1000);
-        $('#connectionModal').modal('hide');
-        setTimeout(function () {
-            window.clearInterval(interval);
-            if (!Page.State.rapi || !Page.State.connected) {
-                Page.setError('<strong>Incorrect</strong> connection parameters.');
-            }
-        }, 15000);
+                Busy.stop();
+            }).error(errorCallback);
+        }).error(errorCallback);
     };
 };
-ConnectionParams.$inject = ['$scope', 'messageBoard'];
-
-// The AngularJS Controller for the listed cartridges
-var Cartridges = function ($scope, messageBoard) {
-    $scope.cartridges = [];
-    $scope.$on('newCartridgesListed', function () {
-        var i;
-        for (i = 0; i < messageBoard.cartridges.length; i = i + 1) {
-            messageBoard.cartridges[i].img = 'http://placehold.it/120x80';
-        }
-        $scope.cartridges = messageBoard.cartridges;
-        setTimeout(function () {
-            $scope.$digest();
-            $("[rel=popover]").popover({
-                animation: true,
-                trigger: 'click',
-                offset: 10,
-                placement: 'top'
-            }).click(function(e) {e.preventDefault()});
-        }, 300);
-    });
-};
-Cartridges.$inject = ['$scope', 'messageBoard'];
-
-
 
 // Main!
 $(function () {
@@ -141,4 +138,13 @@ $(function () {
             $('#showCartridges').html(text);
         });
     });
+    
+    setInterval(function () {
+        $("[rel=popover]").popover({
+            animation: true,
+            trigger: 'click',
+            offset: 10,
+            placement: 'top'
+        }).click(function(e) {e.preventDefault()});
+    }, 1000);
 });
