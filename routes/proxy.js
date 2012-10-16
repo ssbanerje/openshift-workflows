479b3dd8029c512981ff68e4f5954c5cbb01c28a
Subho Banerjee <subs.zero@gmail.com>
2012-10-16 11:52:48 +0530

    proxy returns correct content-type

--- routes/proxy.js
+++ routes/proxy.js
@@ -4,8 +4,9 @@ var request = require('request');
 
 exports.proxify = function (req, res) {
     var options = null;
+    res.contentType('application/json');
     if (req.body.options) {
-        var options = JSON.parse(req.body.options);     
+        var options = JSON.parse(req.body.options);
         request(options, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 res.send(body);
