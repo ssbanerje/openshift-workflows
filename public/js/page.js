b0fe226416c310cf9e305ac8c81160fecb1804d9
Vijayendra Grampurohit <vijayendra.sdm@gmail.com>
2012-10-16 15:56:13 +0530

    Added cartridges images

--- public/js/page.js
+++ public/js/page.js
@@ -15,7 +15,7 @@ var Page = {
         $('#errorPlaceHolder').html('<div class="alert alert-error fade in">' + text
                                     + '<a class="close" data-dismiss="alert" href="#">&times;</a></div>');
     },
-    
+
     spinner: undefined, // Show a spinner to indicate busy status
     startSpinner : function () {
         $('#spinner').html('&nbsp;&nbsp;');
@@ -57,7 +57,7 @@ var ConnectionParams = function ($scope, messageBoard) {
     $scope.password = '';
     $scope.appName = '';
     $scope.namespace = '';
-    
+
     $scope.submit = function () { // Check the connection details and get the cartridge list
         Page.State.connected = false;
         $(".alert").alert('close');
@@ -96,7 +96,80 @@ var Cartridges = function ($scope, messageBoard) {
     $scope.$on('newCartridgesListed', function () {
         var i;
         for (i = 0; i < messageBoard.cartridges.length; i = i + 1) {
-            messageBoard.cartridges[i].img = 'http://placehold.it/120x80';
+           switch(messageBoard.cartridges[i].display_name) {
+              case 'Node.js 0.6':
+                 messageBoard.cartridges[i].img = '/img/icons/nodejs.png';
+                 break;
+              case 'Zend Server 5.6':
+                 messageBoard.cartridges[i].img= '/img/icons/zend.png';
+                 break;
+               case 'Ruby 1.9':
+                  messageBoard.cartridges[i].img= '/img/icons/ruby.png';
+                break;
+
+               case 'JBoss Application Server 7.1':
+                   messageBoard.cartridges[i].img= '/img/icons/jboss.png';
+               break;
+
+               case 'Python 2.6':
+
+                  messageBoard.cartridges[i].img= '/img/icons/python.png';
+                 break;
+
+                case 'Jenkins Server 1.4':
+                  messageBoard.cartridges[i].img= '/img/icons/jenkins.png';
+                 break;
+
+                case 'Ruby 1.8':
+                  messageBoard.cartridges[i].img= '/img/icons/ruby.png';
+                 break;
+
+                case 'JBoss Enterprise Application Platform 6.0':
+                  messageBoard.cartridges[i].img= '/img/icons/jboss.png';
+                 break;
+
+                case 'PHP 5.3':
+                  messageBoard.cartridges[i].img= '/img/icons/php.png';
+                 break;
+                case 'Perl 5.10':
+                  messageBoard.cartridges[i].img= '/img/icons/perl.png';
+                 break;
+                case 'MongoDB NoSQL Database 2.0':
+                  messageBoard.cartridges[i].img= '/img/icons/mongodb.png';
+                 break;
+                case 'Cron 1.4':
+                  messageBoard.cartridges[i].img= '/img/icons/cron.png';
+                 break;
+                case 'MySQL Database 5.1':
+                  messageBoard.cartridges[i].img= '/img/icons/mysql.png';
+                 break;
+                case 'PostgreSQL Database 8.4':
+                  messageBoard.cartridges[i].img= '/img/icons/postgresql.png';
+                 break;
+                case 'HAProxy 1.4':
+                  messageBoard.cartridges[i].img= '/img/icons/haproxy.png';
+                 break;
+
+                case '10gen Mongo Monitoring Service Agent 0.1':
+                  messageBoard.cartridges[i].img= '/img/icons/mongodb.png';
+                 break;
+                case 'phpMyAdmin 3.4':
+                  messageBoard.cartridges[i].img= '/img/icons/php.png';
+                 break;
+                case 'OpenShift Metrics 0.1':
+                  messageBoard.cartridges[i].img= '/img/icons/openshift.png';
+                 break;
+                case 'RockMongo 1.1':
+                  messageBoard.cartridges[i].img= '/img/icons/mongodb.png';
+                 break;
+                case 'Jenkins Client 1.4':
+                  messageBoard.cartridges[i].img= '/img/icons/jenkins.png';
+                 break;
+
+                 default:
+                 messageBoard.cartridges[i].img = 'http://placehold.it/120x80';
+                 break;
+           }
         }
         $scope.cartridges = messageBoard.cartridges;
         setTimeout(function () {
@@ -112,27 +185,27 @@ var Cartridges = function ($scope, messageBoard) {
 };
 Cartridges.$inject = ['$scope', 'messageBoard'];
 
-    
-    
+
+
 // Main!
-$(function () { 
+$(function () {
     // Get connection parameters for PaaS provider
     $('.connectionParam').click(function () {
         $('#connectionModal').modal('show');
     });
-    
+
     // Connect to PaaS provider and get all data
     $('#reconnect').click(function () {
         $('#connection').css('color', '#0d0');
     });
-    
+
     // All features for node tags
     $('.node').hover(function () {
         $(this).find('.controller').show();
     }, function () {
         $(this).find('.controller').hide();
     });
-    
+
     // Show Cartridges
     $('#showCartridges').click(function () {
         $('#listCartridges').slideToggle('slow', function () {
